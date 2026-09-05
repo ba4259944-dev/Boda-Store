beeb1b73808e9003b47521cecd9a381de287cfdb
ba4259944-dev <ba4259944@gmail.com>
2026-09-06 00:12:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const themeToggleBtn = document.getElementById('theme-toggle');
 themeToggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('light-theme');
 });
+const scrollBottomBtn = document.getElementById('scrollBottomBtn');
+
+if (scrollBottomBtn) {
+    scrollBottomBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: 'smooth'
+        });
+    });
+}
